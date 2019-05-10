6ad879033720e5a7065cdff1f7ea023fd5d45e44
Omar del Valle <omar@odelvalle.com>
2019-05-10 14:32:12 +0200

    fixes petstore definition

--- src/definition-test.js
+++ src/definition-test.js
@@ -1,12 +1,6 @@
 'use strict';
 
-const colors = require('colors');
-
-colors.setTheme({
-  info: 'green',
-  error: 'red',
-  warn: 'yellow'
-});
+const colors = require('colors/safe');
 
 module.exports.definitionTestsPassed = (endpoint, security) => {
   if (isNotImplemented(endpoint.def.responses)) return true;
@@ -16,34 +10,34 @@ module.exports.definitionTestsPassed = (endpoint, security) => {
   switch (endpoint.method) {
   case 'get':
     resp = ['200', '202', '206'];
-    testPassed = accept(endpoint.def.responses, resp, 
+    testPassed = accept(endpoint.def.responses, resp,
       'Should be contain success (' + resp.join() + ') response.') && testPassed;
 
     break;
   case 'post':
     resp = ['400'];
-    testPassed = accept(endpoint.def.responses, resp, 
+    testPassed = accept(endpoint.def.responses, resp,
       'Should be contain bad request (' + resp.join() + ') response.') && testPassed;
 
     resp = ['200', '201', '202', '204', '206'];
-    testPassed = accept(endpoint.def.responses, resp, 
+    testPassed = accept(endpoint.def.responses, resp,
       'Should be contain success (' + resp.join() + ') response.') && testPassed;
 
     break;
   case 'patch':
   case 'put':
     resp = ['400'];
-    testPassed = accept(endpoint.def.responses, resp, 
+    testPassed = accept(endpoint.def.responses, resp,
       'Should be contain bad request (' + resp.join() + ') response.') && testPassed;
 
     resp = ['200', '202', '204', '206'];
-    testPassed = accept(endpoint.def.responses, resp, 
+    testPassed = accept(endpoint.def.responses, resp,
       'Should be contain success (' + resp.join() + ') response.') && testPassed;
 
     break;
   case 'delete':
     resp = ['200', '202', '204'];
-    testPassed = accept(endpoint.def.responses, resp, 
+    testPassed = accept(endpoint.def.responses, resp,
       'Should be contain success (' + resp.join() + ') response.') && testPassed;
   }
 
@@ -53,25 +47,36 @@ module.exports.definitionTestsPassed = (endpoint, security) => {
     testPassed = writeMessage(consumes !== undefined, 'Should be contain consumes (accept) definition.') && testPassed;
   } else if (produces === undefined) {
     resp = ['200', '202', '206'];
-    testPassed = accept(endpoint.def.responses, resp, 
+    testPassed = accept(endpoint.def.responses, resp,
       'Should be contain produces (content-type) definition.') && testPassed;
   }
 
   if (security) {
-    testPassed = accept(endpoint.def.responses, ['401'], 
-      'Should be contain (401) response.') && testPassed;
+    testPassed = accept(endpoint.def.responses, ['401'],
+      'Security definition, should be contain (401) response.') && testPassed;
   }
 
-  if (endpoint.def.parameters && 
-    endpoint.def.parameters.some(param => param.in === 'path') && !endpoint.def['x-pm-test-ignore404']) {
-    testPassed = accept(endpoint.def.responses, ['404'], 
-      'Parameters in PATH require not found (404) response.') && testPassed;
+  if (endpoint.def.parameters && endpoint.def.parameters.some(param => ['path', 'query'].includes(param.in) &&
+      param.required) && !endpoint.def['x-pm-test-ignore404']) {
+    testPassed = accept(endpoint.def.responses, ['404'],
+      'Parameters required in PATH or QUERY, should be contain not found (404) response.') && testPassed;
+  }
+
+  if (endpoint.def.parameters && endpoint.def.parameters.some(param => param.in === 'query' && param.required)) {
+    process.stdout.write(colors.yellow('─ ') +
+      'Parameters required in QUERY, maybe should be use \'in: path\' parameters\n');
+  }
+
+  if (endpoint.def.parameters && endpoint.def.parameters.some(param => param.enum !== undefined ||
+      (param.type === 'array' && param.items.enum !== undefined))) {
+    testPassed = accept(endpoint.def.responses, ['400'],
+      'Enum parameters in PATH or QUERY, should be contain bad request (400) response.') && testPassed;
   }
 
   const queryParams = endpoint.def.parameters.filter(param => param.in === 'query');
   queryParams.forEach(param => {
-    testPassed = isLowerCase(param.name, 
-      'Query string parameter [' + param.name + '] Should be lower case.') && testPassed;        
+    testPassed = isLowerCase(param.name,
+      'Query string parameter [' + param.name + '] should be lower case.') && testPassed;
   });
 
   return testPassed;
@@ -81,7 +86,7 @@ function isNotImplemented (responses) {
   return Object.keys(responses).some(prop => {
     const notImplemented = Number(prop) === 501;
     // @ts-ignore
-    if (notImplemented) process.stdout.write(colors.warn('√ ') + 'Is not implemented.\n');
+    if (notImplemented) process.stdout.write('√ ' + colors.inverse('Is not implemented.\n'));
 
     return notImplemented;
   });
@@ -110,6 +115,6 @@ function isLowerCase (str, message) {
  */
 function writeMessage (result, message) {
   // @ts-ignore
-  process.stdout.write((result ? colors.info('√ ') : colors.error('† ')) + message + '\n');
+  process.stdout.write((result ? colors.green('√ ') : colors.red.bold('┼ ')) + message + '\n');
   return result;
 }
